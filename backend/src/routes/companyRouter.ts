3e407f6703296eeb2d85256af0f0e9e5b26b455f
shifat97 <shifatbinreza2@gmail.com>
2025-09-22 01:46:54 +0600

    backend: add validation data validation middleware

--- backend/src/routes/companyRouter.ts
+++ backend/src/routes/companyRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { companyController } from '@/controllers';
+import { validatePayload } from '@/middlewares/validatePayload';
+import { CreateCompanySchema } from '@/schemas';
 
 const router = Router();
 
-router.post('/', companyController.createCompany);
+router.post(
+  '/',
+  validatePayload(CreateCompanySchema),
+  companyController.createCompany,
+);
 router.get('/', companyController.getCompanies);
 
 export default router;
