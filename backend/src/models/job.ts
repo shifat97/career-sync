e7e9d8439dfe5d1ee732b05101c28b4089efe007
shifat97 <shifatbinreza2@gmail.com>
2025-10-02 22:52:27 +0600

    backend: add job model

---
+++ backend/src/models/job.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+import type { Job } from '@/schemas';
+import { EmploymentType, WorkPlaceType, JobStatus } from '@/types/index';
+
+const JobSchema = new Schema<Job>(
+  {
+    title: { type: String, required: true },
+    jobDescription: { type: String, required: true },
+    openingDate: { type: Date, required: true },
+    applicationDeadline: { type: Date, required: true },
+    department: { type: String, required: true },
+    location: { type: String, required: true },
+    company: Schema.Types.ObjectId,
+    employmentType: { type: String, enum: EmploymentType, required: true },
+    workPlaceType: { type: String, enum: WorkPlaceType, required: true },
+    compensation: { type: String, required: true },
+    skills: [{ type: String }],
+    vacancy: { type: Number, required: true },
+    tags: [{ type: String }],
+    status: { type: String, enum: JobStatus, default: 'draft' },
+    deletedAt: { type: Date, default: null },
+  },
+  { timestamps: true, virtuals: true },
+);
+
+export const JobModel = model('Job', JobSchema);
