a0d4e6c9d550c37d1d92a2a1bd6f33b2e247d9cd
TasinLunatic <ahmedtasin207@gmail.com>
2025-10-03 20:50:19 +0800

    admin:add routing setup

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import { Button } from './components/ui/button';
+import { Outlet } from 'react-router';
 
 function App() {
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button>Click me</Button>
-      </div>
+      <Outlet />
     </>
   );
 }
