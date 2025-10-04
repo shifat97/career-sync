3fe179a807f19634d4f6b48bb7c378201cde4b06
shifat97 <shifatbinreza2@gmail.com>
2025-10-04 12:48:29 +0600

    backend: add endpoint to get all jobs of a company

--- backend/src/controllers/jobController.ts
+++ backend/src/controllers/jobController.ts
@@ -5,3 +5,13 @@ export const createJob = async (req: Request, res: Response) => {
   const newJob = await jobServices.createJob(req.body);
   res.status(201).json(newJob.toJSON());
 };
+
+export const getAllJobs = async (req: Request, res: Response) => {
+  const { companyId } = req.params;
+  const allJobs = await jobServices.getAllJobs(companyId);
+
+  res.status(200).json({
+    data: allJobs.filter((jobs) => jobs.deletedAt === null),
+    status: 'success',
+  });
+};
