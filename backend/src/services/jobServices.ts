00f59cb079e84b65a0d3d312650c4f12bf901ea1
shifat97 <shifatbinreza2@gmail.com>
2025-10-04 14:34:11 +0600

    backend: add paginated response for job data

--- backend/src/services/jobServices.ts
+++ backend/src/services/jobServices.ts
@@ -1,12 +1,33 @@
 import { JobModel } from '@/models';
-import type { CreateJob } from '@/schemas';
+import type { CreateJob, Job } from '@/schemas';
+import { PaginatedResponse } from '@/types';
 
 export const createJob = async (jobPayload: CreateJob) => {
   const newJob = await JobModel.create(jobPayload);
   return newJob;
 };
 
-export const getAllJobs = async (id: string) => {
-  const allJobs = await JobModel.find({ company: id }).populate('company');
-  return allJobs;
+export const getAllJobs = async (
+  id: string,
+  { page = 0, limit = 10 }: { page: number; limit: number },
+): Promise<PaginatedResponse<Job>> => {
+  const [jobs, total] = await Promise.all([
+    JobModel.find({ company: id, deletedAt: null })
+      .populate('company')
+      .skip(page * limit)
+      .limit(limit)
+      .lean()
+      .exec(),
+    JobModel.countDocuments({
+      company: id,
+      deletedAt: null,
+    }).exec(),
+  ]);
+
+  return {
+    items: jobs,
+    total,
+    page,
+    limit,
+  };
 };
