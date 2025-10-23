a5f54618884c86cd9d9031ec7355992601768d4a
shifat97 <shifatbinreza2@gmail.com>
2025-10-23 22:33:21 +0600

    admin: change port to hard coded number

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -2,14 +2,13 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from './src/configs/envConfig';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
-    port: Number(dotenv.PORT),
+    port: 3359,
   },
   resolve: {
     alias: {
